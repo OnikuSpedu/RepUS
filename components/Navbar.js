08feb44a12f28b11be4e5e95b3035b4a115b17aa
OnikuSpedu <srakibnyc1@gmail.com>
2021-06-20 22:34:14 -0400

    Added glassmorphic sidebar and call to action

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="w-full flex items-center h-16 px-4 text-lg font-bold">
+            RepUS
+        </div>
+    )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,40 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import bgImage from '../public/bg.jpg'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col min-h-screen">
       <Head>
-        <title>Create Next App</title>
+        <title>RepUS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <Image
+        alt="Mountains"
+        src={bgImage}
+        layout="fill"
+        objectFit="cover"
+        className="-z-1"
+        quality={100}
+      />
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className="flex-auto flex flex-col lg:flex-row lg:flex-wrap">
+        <div className="flex-1 lg:max-w-4xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border border-gray-300">
+          <Navbar />
+          <div className="p-4 leading-snug flex-auto flex items-center text-5xl font-bold">
+            Find Your Elected Officials
+          </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <form>
+            
+          </form>
+        </div>
+        <div className="flex-auto">
+          Hello
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
