84f914aa502985b53dfa16fbfecae88f2520a083
OnikuSpedu <srakibnyc1@gmail.com>
2021-06-23 13:40:15 -0400

    Removed sleep function used for testing

--- components/Results.tsx
+++ components/Results.tsx
@@ -70,12 +70,4 @@ function Results({ query }: ResultsProps) {
   );
 }
 
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
 export default Results;
