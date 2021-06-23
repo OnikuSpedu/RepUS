3a230a02ffe86c097292d503984e32844b23134b
OnikuSpedu <srakibnyc1@gmail.com>
2021-06-23 16:28:04 -0400

    Added contact buttons to card

--- components/ResultCard.tsx
+++ components/ResultCard.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
-import Image from "next/image";
+import { GlobeAltIcon, PhoneIcon, MailIcon } from "@heroicons/react/outline";
+import { FaYoutube, FaTwitter, FaFacebook } from "react-icons/fa";
+
+interface Channel {
+  type: string;
+  id: string;
+}
 
 interface ResultCardProps {
   office: string;
@@ -9,7 +15,7 @@ interface ResultCardProps {
   photo?: string;
   address?: object[];
   urls?: string[];
-  channels?: object[];
+  channels?: Channel[];
   emails?: string[];
   phones?: string[];
 }
@@ -25,9 +31,43 @@ function ResultCard({
   emails,
   phones,
 }: ResultCardProps) {
+  const channelRenderer = (type, id) => {
+    switch (type) {
+      case "Facebook":
+        return (
+          <a
+            href={`https://www.facebook.com/${id}`}
+            className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full hover:bg-gray-400"
+          >
+            <FaFacebook className="w-6 h-6 text-gray-700" />
+          </a>
+        );
+      case "Twitter":
+        return (
+          <a
+            href={`https://www.twitter.com/${id}`}
+            className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full hover:bg-gray-400"
+          >
+            <FaTwitter className="w-6 h-6 text-gray-700" />
+          </a>
+        );
+      case "YouTube":
+        return (
+          <a
+            href={`https://www.youtube.com/user/${id}`}
+            className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full hover:bg-gray-400"
+          >
+            <FaYoutube className="w-6 h-6 text-gray-700" />
+          </a>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div
-      className={`flex flex-col gap-6 md:flex-row w-full max-w-3xl p-6 my-2 bg-white shadow-2xl rounded-xl border-l-8 ${
+      className={`flex flex-col gap-6 sm:flex-row w-full max-w-3xl p-6 my-2 bg-white shadow-2xl rounded-xl border-l-8 ${
         party == "Democratic Party" && "border-blue-500"
       } ${party == "Republican Party" && "border-red-500"} ${
         party != "Republican Party" &&
@@ -36,7 +76,7 @@ function ResultCard({
       }`}
     >
       <div className="">
-        <div className="overflow-hidden w-36 h-36 square">
+        <div className="w-48 h-48 overflow-hidden sm:w-44 sm:h-44">
           <img
             src={photo ? photo : "/person-placeholder.jpg"}
             alt="Picture of the official"
@@ -46,8 +86,41 @@ function ResultCard({
       </div>
       <div className="flex flex-col flex-auto">
         <div className="text-lg uppercase">{office}</div>
-        <div className="text-2xl font-bold">{name}</div>
-        <div className="mt-4 text-lg">{party}</div>
+        <div className="mt-4 text-3xl font-bold">{name}</div>
+        <div className="mt-1 text-lg">{party}</div>
+        <div className="flex flex-row flex-wrap gap-3 mt-6">
+          {urls &&
+            urls.map((url) => (
+              <a
+                href={url}
+                className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full hover:bg-gray-400"
+              >
+                <GlobeAltIcon className="w-6 h-6 text-gray-700" />
+              </a>
+            ))}
+          {emails &&
+            emails.map((email) => (
+              <a
+                href={`mailto:${email}`}
+                className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full hover:bg-gray-400"
+              >
+                <MailIcon className="w-6 h-6 text-gray-700" />
+              </a>
+            ))}
+          {phones &&
+            phones.map((phone) => (
+              <a
+                href={`tel:${phone}`}
+                className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full hover:bg-gray-400"
+              >
+                <PhoneIcon className="w-6 h-6 text-gray-700" />
+              </a>
+            ))}
+          {channels &&
+            channels.map(({ type, id }) => (
+              <React.Fragment>{channelRenderer(type, id)}</React.Fragment>
+            ))}
+        </div>
       </div>
     </div>
   );
