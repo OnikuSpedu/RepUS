a28f1e9db241a438f5a6bc1626e4c7e2fe63f63c
OnikuSpedu <srakibnyc1@gmail.com>
2021-06-23 00:35:32 -0400

    Added left border to indicate party

--- components/ResultCard.tsx
+++ components/ResultCard.tsx
@@ -14,7 +14,7 @@ interface ResultCardProps {
 
 function ResultCard({office, name, party, photo, address, urls, channels, emails, phones} : ResultCardProps) {
     return (
-        <div className="flex flex-col max-w-3xl p-6 my-4 bg-white shadow-2xl rounded-xl">
+        <div className={`flex flex-col max-w-3xl p-6 my-4 bg-white shadow-2xl rounded-xl border-l-8 ${(party == "Democratic Party" && "border-blue-500")} ${(party == "Republican Party" && "border-red-500")} ${((party != "Republican Party" && party != "Democratic Party") && "border-gray-200")}`}>
             <div className="text-lg uppercase">{office}</div>
             <div className="text-2xl font-bold">{name}</div>
             <div className="mt-4 text-lg">{party}</div>
